b80eb14f9a9f5e4a498b7f0e3e87431e82f858cc
Étienne <etienne.roy@recitad.ca>
2019-11-21 14:29:28 -0500

    mise à jour du répertoire

--- src/db/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-function xmldb_mod_recitcahiercanada_uninstall() {
-    // Moodle uses the file install.xml to drop all the tables related to this plugin
-}
