1b6a7c0ef073193a7619ca9eabb94908a264206c
Gustavo Bazzo <gcbazzo@gmail.com>
2019-12-04 15:46:54 -0500

    Ajout nouveau code d'intégration
    Ajout nouveau code d'intégration pour permettre l'exportation/importation du cahier de traces dans la même base de données et ailleurs
    Modifier l'exportation du cahier de traces et ne pas inclure les données des usagers;

--- src/db/uninstall.php
+++ src/db/uninstall.php
@@ -4,4 +4,5 @@ defined('MOODLE_INTERNAL') || die();
 
 function xmldb_mod_recitcahiercanada_uninstall() {
     // Moodle uses the file install.xml to drop all the tables related to this plugin
+    return true;
 }
