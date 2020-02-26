08ed0a8f552f127242653d15e434b762d5ded0bd
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-02-26 14:01:33 -0500

    $plugin->release = 'R5-2020022900'

--- src/version.php
+++ src/version.php
@@ -22,12 +22,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020020500;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2020022900;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;    // Requires this Moodle version
 $plugin->component = 'mod_recitcahiercanada';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
 $plugin->dependencies = [                                                                                                           
-    'local_recitcommon' => '2020020500'
+    'local_recitcommon' => '2020022900'
 ];
-$plugin->release = 'R2 - 2020021700'; 
+$plugin->release = 'R5-2020022900'; 
 $plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
