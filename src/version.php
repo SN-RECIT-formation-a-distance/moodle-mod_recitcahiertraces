7db2b5b02e73a6ab642e79599cebf3a7d0364c43
Gustavo Bazzo <gcbazzo@gmail.com>
2022-02-10 14:28:59 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -22,12 +22,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021092400;        // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018050800;    // Requires this Moodle version
+$plugin->version   = 2022020900;        // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'mod_recitcahiercanada';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
 $plugin->dependencies = [    
-    'local_recitcommon' => 2021092400
+    'local_recitcommon' => 2022020900
 ];
-$plugin->release = 'R14-V1.2'; 
+$plugin->release = 'v1.15.0'; 
 $plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
