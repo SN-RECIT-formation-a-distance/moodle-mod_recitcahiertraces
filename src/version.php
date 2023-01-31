52b485a6503baa5de4b15d17c27ebf04dbe7237d
Gustavo Bazzo <gcbazzo@gmail.com>
2023-01-31 12:06:16 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -26,5 +26,5 @@ $plugin->version   = 2023012300;        // The current module version (Date: YYY
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'mod_recitcahiertraces';        // Full name of the plugin (used for diagnostics)
 $plugin->release = 'v2.3.0-beta'; 
-$plugin->supported = [39, 401];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
+$plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 $plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
